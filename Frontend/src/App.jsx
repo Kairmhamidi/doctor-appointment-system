4c27439fc66c2ed75fe7525dcc691928f1f928a8
unknown <karimjanhamidi10@gmail.com>
2025-03-17 01:59:49 -0700

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='w-full h-full flex justify-center items-center flex-col'>
+      <h1 className="text-3xl font-bold bg-sky-400">i am done</h1>
+      <h2 className=''>hamidi</h2>
+    </div>
+  )
+}
+
+export default App
